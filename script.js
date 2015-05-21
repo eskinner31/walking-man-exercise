c78a48c9612d331e67159a7792f3aec363713bd4
Eugene Skinner <eskinfrn@gmail.com>
2015-05-21 14:25:34 -0600

    Got the stick figure to move with the click

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-var stickFigure = document.getElementById("sticky");
+  var viewport = document.defaultView;
+  var stickFigure = document.getElementById("sticky");
+  stickFigure.style.position = "relative";
+  stickFigure.style.left ="0px";
+//document.body.style.width = "1000px";
 
 
-stickFigure.addEventListener('click' function(){
-  
-}
+viewport.addEventListener('click',function(){
+  stickFigure.style.left = parseInt(stickFigure.style.left) + 10 + "px";
+})
