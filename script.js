d523ab75abcf943a83c170f7b6d74b9691828455
Eugene Skinner <eskinfrn@gmail.com>
2015-05-21 15:52:04 -0600

    got the forward and back motion to function properly

--- script.js
+++ script.js
@@ -2,9 +2,25 @@
   var stickFigure = document.getElementById("sticky");
   stickFigure.style.position = "relative";
   stickFigure.style.left ="0px";
+  var logicValue = true;
 //document.body.style.width = "1000px";
+  var move = function(){
+    if (logicValue == true){
+      stickFigure.style.left = parseInt(stickFigure.style.left) + 10 + "px";
+      if(parseInt(stickFigure.style.left) >= 1000){
+        logicValue = false;
+      }
+      setTimeout(move,200);
+    }
+    if (logicValue == false){
+      stickFigure.style.left = parseInt(stickFigure.style.left) - 10 + "px";
+      if(parseInt(stickFigure.style.left) <= 0){
+        logicValue = true;
+      }
+      setTimeout(move,200);
+    }
+  }
 
-
-viewport.addEventListener('click',function(){
-  stickFigure.style.left = parseInt(stickFigure.style.left) + 10 + "px";
+viewport.addEventListener('load',function(){
+  move();
 })
