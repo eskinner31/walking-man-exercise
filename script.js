7719313075afb1c90352b08b5448f184e988c5ad
Eugene Skinner <eskinfrn@gmail.com>
2015-05-21 19:50:41 -0600

    animation complete

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
   var viewport = document.defaultView;
   var stickFigure = document.getElementById("sticky");
   stickFigure.style.position = "relative";
-  stickFigure.style.left ="0px";
+  stickFigure.style.left ="0px";;
   var logicValue = true;
+
 //document.body.style.width = "1000px";
   var move = function(){
     if (logicValue == true){
       stickFigure.style.left = parseInt(stickFigure.style.left) + 10 + "px";
       if(parseInt(stickFigure.style.left) >= 1000){
         logicValue = false;
+        stickFigure.classList.add("flip");
       }
-      setTimeout(move,200);
     }
     if (logicValue == false){
       stickFigure.style.left = parseInt(stickFigure.style.left) - 10 + "px";
       if(parseInt(stickFigure.style.left) <= 0){
         logicValue = true;
+        stickFigure.classList.remove("flip");
       }
-      setTimeout(move,200);
     }
   }
 
 viewport.addEventListener('load',function(){
-  move();
+  setInterval(move,200);
 })
